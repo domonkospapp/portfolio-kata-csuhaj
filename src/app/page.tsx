4555ceadeab6df05d4d5c97e3653bf9da57ec51a
Domonkos Papp <domonkos.papp@tronic.digital>
2024-07-09 08:22:44 +0200

    Make fixed title gray

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:py-16 pb-8 sm:px-8 px-4">
-      <p className="font-cairo text-7xl md:text-[9rem] -z-10 fixed top-5 md:top-20 text-orange-300">
+      <p className="font-cairo text-7xl md:text-[9rem] -z-10 fixed top-5 md:top-20 text-gray-400">
         KATA
       </p>
       <h1 className="text-6xl md:text-9xl text-center font-bold mt-8 mb-4">
